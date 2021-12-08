61ef171256beea74e3d103bf755fed323fa680ef
Lovianno <loviannods10rpl2@gmail.com>
2021-12-08 09:01:59 +0700

    initial commit

--- kelinci/index.js
+++ kelinci/index.js
@@ -1,9 +1,35 @@
+// body
+const body = document.querySelector("body");
+
 const burger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav ul");
-const body = document.querySelector("body");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("transform");
   nav.classList.toggle("slide");
   body.classList.toggle("black");
+});
+
+const dropbar = document.querySelector(".dropbar");
+const ulDrop = document.querySelector(".ulDrop");
+const profil = document.querySelector(".profil");
+
+dropbar.addEventListener("click", function () {
+  ulDrop.style.display = "block";
+  dropbar.style.transition = "0.5s";
+  dropbar.style.transform = "rotate(-230deg)";
+});
+
+profil.addEventListener("mouseleave", function () {
+  ulDrop.style.display = "none";
+  dropbar.style.transition = "0.5s";
+  dropbar.style.transform = "rotate(-50deg)";
+});
+
+// nav menu
+const menu = document.querySelectorAll(".menu li a");
+menu.forEach(function (menuSemua) {
+  menuSemua.addEventListener("mouseleave", function () {
+    menuSemua.style.transition = "1s";
+  });
 });
